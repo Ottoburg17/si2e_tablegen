51c2f0ee6c372574509d4da8cb776449a9f8171d
Fehér Mátyás <waldorfosmatyi@gmail.com>
2023-02-16 19:58:09 +0100

    Törlésgomb hozzáadva

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const gyumolcsok = [
+    { name: 'szilva', quantity: 35, price: 8 },
+    { name: 'alma', quantity: 45, price: 8.3 },
+    { name: 'körte', quantity: 25, price: 9.5 },
+    { name: 'barack', quantity: 37, price: 12 },
+  ];
+
+  const tbody = document.querySelector('#tbody');
+
+  function generateTbody() {
+    gyumolcsok.forEach((gyumolcs) => {
+        console.log(gyumolcs.name)
+        
+        let tr = document.createElement('tr');
+        let tdName = document.createElement('td');
+        let tdQuantity = document.createElement('td');
+        let tdPrice = document.createElement('td');
+
+        tdName.textContent = gyumolcs.name;
+        tdQuantity.textContent = gyumolcs.price;
+        tdPrice.textContent = gyumolcs.price;
+
+        tbody.append(tr);
+        tr.append(tdName);
+        tr.append(tdQuantity);
+        tr.append(tdPrice);
+        tr.append(generateTdDelete());
+    });
+
+  }
+  generateTbody();
+
+  function generateTdDelete() {
+    let td = document.createElement('td');
+    let button = document.createElement('button');
+    button.textContent = "Törlés";
+    button.classList = "btn btn-warning";
+    td.append(button);
+    return td;
+  }
